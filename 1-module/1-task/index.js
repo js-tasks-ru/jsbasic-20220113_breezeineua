61630f22f97181d5f2d532b08d7ea92f6d65f122
kateriii <breezeineua@gmail.com>
2022-01-25 23:24:06 +0200

    fix PR comments

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -2,7 +2,7 @@ function factorial(n) {
   if (n == 0) {
     return 1;
   } 
-  multi = 1;
+ let multi = 1;
   for (let i = n; i > 1; i--) {
     multi = multi * i;
   }
--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,7 +1,4 @@
 function checkSpam(str) {
   let lowerStr = str.toLowerCase();
-  if (lowerStr.includes("1xbet") || lowerStr.includes("xxx")) {
-    return true;
-  } 
-  return false;
+  return lowerStr.includes("1xbet") || lowerStr.includes("xxx")
 }
